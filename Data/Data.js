bf3fa1bea31aeb71fe2f95781d19fc642067bad6
Kawsar Ahammed <115506566+thebegjoker@users.noreply.github.com>
2023-10-02 12:57:47 -0700

    Add files via upload

---
+++ Data/Data.js
@@ -0,0 +1,347 @@
+export const links =[
+    {
+        name:'Home',
+        path:'/'
+    },
+    {
+        name:'About',
+        path:'/about'
+    },
+    {
+        name:'Gallery',
+        path:'/gallery'
+    },
+    {
+        name:'Plans',
+        path:'/plans'
+    },
+    {
+        name:'Trainers',
+        path:'/trainers'
+    },
+    {
+        name:'Contact',
+        path:'/contact'
+    },
+];
+
+
+
+
+
+import programImage1 from '../assets/p1.png'
+import programImage2 from '../assets/p2.png'
+import programImage3 from '../assets/p3.png'
+import programImage4 from '../assets/p4.png'
+export const progarms =[
+  
+    {
+        id:1,
+        img: programImage1,
+        title: 'Programs One',
+        des: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam!',
+        path: '/programs/111'
+    },
+    {
+        id:2,
+        img: programImage2,
+        title: 'Programs Two',
+        des: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam!',
+        path: '/programs/112'
+    },
+    {
+        id:3,
+        img: programImage3,
+        title: 'Programs Three',
+        des: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam!',
+        path: '/programs/113'
+    },
+    {
+        id:4,
+        img: programImage4,
+        title: 'Programs Foure',
+        des: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam!',
+        path: '/programs/114'
+    },
+];
+
+
+
+
+
+
+
+
+import valueImage1 from '../assets/v1.png'
+import valueImage2 from '../assets/v2.png'
+import valueImage3 from '../assets/v3.png'
+export const values =[
+  
+    {
+        id:1,  
+        img: valueImage1,
+        title: 'Value One',
+        des: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam!',       
+    },
+    {
+        id:2,   
+        img: valueImage2,
+        title: 'Value Two',
+        des: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam!',       
+    },
+    {
+        id:3,   
+        img: valueImage3,
+        title: 'Value Three',
+        des: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam!',       
+    }, 
+];
+
+
+export const faqs =[
+    {
+        id:1,   
+        que: 'How opten should i exercise?',        
+        ans: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam!',       
+    }, 
+    {
+        id:2,   
+        que: 'How opten should i exercise?',        
+        ans: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam!',       
+    }, 
+    {
+        id:3,   
+        que: 'How opten should i exercise?',        
+        ans: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam!',       
+    }, 
+    {
+        id:4,   
+        que: 'How opten should i exercise?',        
+        ans: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam!',       
+    }, 
+    {
+        id:5,   
+        que: 'How opten should i exercise?',        
+        ans: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam!',       
+    }, 
+    {
+        id:6,   
+        que: 'How opten should i exercise?',        
+        ans: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam!',       
+    }, 
+];
+
+
+
+
+
+import testimonialImage1 from '../assets/ava1.jpg'
+import testimonialImage2 from '../assets/ava2.jpg'
+import testimonialImage3 from '../assets/ava3.png'
+import testimonialImage4 from '../assets/ava4.jpg'
+import testimonialImage5 from '../assets/ava5.png'
+import testimonialImage6 from '../assets/ava6.png'
+export const testimonials =[
+    {
+        id:1,   
+        img: testimonialImage1,
+        name: 'Anna Lia',        
+        quote: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam! Lorem ipsum dolor sit, amet consectetur Ipsa, laboriosam!', 
+        job: 'Student'      
+    }, 
+    {
+        id:2,   
+        img: testimonialImage2,
+        name: 'Habib Akram',        
+        quote: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam! Lorem ipsum dolor sit, amet consectetur Ipsa, laboriosam!', 
+        job: 'Student'      
+    }, 
+    {
+        id:3,   
+        img: testimonialImage3,
+        name: 'Adriana Ava',        
+        quote: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam! Lorem ipsum dolor sit, amet consectetur Ipsa, laboriosam!', 
+        job: 'Student'      
+    }, 
+    {
+        id:4,   
+        img: testimonialImage4,
+        name: 'Abraham Linkon',        
+        quote: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam! Lorem ipsum dolor sit, amet consectetur Ipsa, laboriosam!', 
+        job: 'Student'      
+    }, 
+    {
+        id:5,   
+        img: testimonialImage5,
+        name: 'Tasnia Farin',        
+        quote: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam! Lorem ipsum dolor sit, amet consectetur Ipsa, laboriosam!', 
+        job: 'Student'      
+    }, 
+    {
+        id:6,   
+        img: testimonialImage6,
+        name: 'Jhon Doe',        
+        quote: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, laboriosam! Lorem ipsum dolor sit, amet consectetur Ipsa, laboriosam!', 
+        job: 'Student'      
+    }, 
+];
+
+
+
+
+
+
+import galleryImage1 from '../assets/gallery2.jpg'
+import galleryImage2 from '../assets/gallery8.jpg'
+import galleryImage3 from '../assets/gallery3.jpg'
+import galleryImage4 from '../assets/gallery4.jpg'
+import galleryImage5 from '../assets/gallery5.jpg'
+import galleryImage6 from '../assets/gallery6.jpg'
+import galleryImage7 from '../assets/gallery7.jpg'
+import galleryImage8 from '../assets/gallery1.jpg'
+export const gallery =[
+    {
+        id: 1,
+        img:galleryImage1
+    },
+    {
+        id: 2,
+        img:galleryImage2
+    },
+    {
+        id: 3,
+        img:galleryImage3
+    },
+    {
+        id: 4,
+        img:galleryImage4
+    },
+    {
+        id: 5,
+        img:galleryImage5
+    },
+    {
+        id: 6,
+        img:galleryImage6
+    },
+    {
+        id: 7,
+        img:galleryImage7
+    },
+    {
+        id: 8,
+        img:galleryImage8
+    },
+];
+
+
+
+
+
+export const plans =[
+    {
+        id: 1,
+        name: 'Basic',
+        des:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, quaerat!',
+        price: '10.99',
+        services:[
+            {feature: "You Feature 2", available: true },
+            {feature: "You Feature 2", available: true },
+            {feature: "You Feature 2", available: true },
+            {feature: "You Feature 2", available: true },                 
+            {feature: "You Feature 2", available: false },
+            {feature: "You Feature 2", available: false },
+            {feature: "You Feature 2", available: false },
+            
+        ]
+    },
+    {
+        id: 2,
+        name: 'Gold',
+        des:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, quaerat!',
+        price: '60.99',
+        services:[
+            {feature: "You Feature 3", available: true },
+            {feature: "You Feature 3", available: true },
+            {feature: "You Feature 3", available: true },
+            {feature: "You Feature 3", available: true },
+            {feature: "You Feature 3", available: true },   
+            {feature: "You Feature 3", available: true },
+            {feature: "You Feature 3", available: true },        
+            {feature: "You Feature 3", available: true },           
+     
+        ]
+    },
+    {
+        id: 3,
+        name: 'Basic',
+        des:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, quaerat!',
+        price: '20.99',
+        services:[
+            {feature: "You Feature 1", available: true },
+            {feature: "You Feature 1", available: true },
+            {feature: "You Feature 1", available: true },
+            {feature: "You Feature 1", available: false },
+            {feature: "You Feature 1", available: true },
+            {feature: "You Feature 1", available: false },
+            {feature: "You Feature 1", available: true },     
+        ]
+    },
+];
+
+
+
+
+
+
+
+import trainersImage1 from '../assets/trainer1.jpg'
+import trainersImage2 from '../assets/trainer2.jpg'
+import trainersImage3 from '../assets/trainer3.jpg'
+import trainersImage4 from '../assets/trainer4.jpg'
+import trainersImage5 from '../assets/trainer5.jpg'
+import trainersImage6 from '../assets/trainer6.jpg'
+export const trainers =[
+    {
+        id: 1,
+        name: 'Mr Been',
+        img:trainersImage1,
+        job: 'Trainer',
+        Email:'',        
+    },
+    {
+        id: 1,
+        name: 'Mr Been',
+        img:trainersImage2,
+        job: 'Trainer',
+        Email:'',  
+    },
+    {
+        id: 1,
+        name: 'Mr Been',
+        img:trainersImage3,
+        job: 'Trainer',
+        Email:'',  
+    },
+    {
+        id: 1,
+        name: 'Mr Been',
+        img:trainersImage4,
+        job: 'Trainer',
+        Email:'',  
+    },
+    {
+        id: 1,
+        name: 'Mr Been',
+        img:trainersImage5,
+        job: 'Trainer',
+        Email:'',  
+    },
+    {
+        id: 1,
+        name: 'Mr Been',
+        img:trainersImage6,
+        job: 'Trainer',
+        Email:'',  
+    },
+];
